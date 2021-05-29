ac1f4194a34d2f2190547837b3827335f5918f33
Ådne E. Moldesæter <stressmannen@live.no>
2021-05-29 12:57:48 +0200

    Added basic weight entry list with list items and mock data

--- lib/ui/pages/weight_entry_register.dart
+++ lib/ui/pages/weight_entry_register.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:weight_tracking/models/weight_entry.dart';
+import 'package:weight_tracking/ui/widgets/weight_entry_list_item.dart';
 
 class WeightEntruRegister extends StatelessWidget {
+  List<WeightEntry> getData() {
+    return [
+      WeightEntry(
+        date: DateTime.parse('2021-05-27'),
+        weight: 87.2,
+        cheatDay: true,
+      ),
+      WeightEntry(
+        date: DateTime.parse('2021-05-28'),
+        weight: 87.7,
+      ),
+      WeightEntry(
+        date: DateTime.parse('2021-05-29'),
+        weight: 87.6,
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Weight entry register'),
+    final data = getData();
+
+    return ListView.separated(
+      itemCount: data.length,
+      separatorBuilder: (ctx, i) => Divider(),
+      itemBuilder: (ctx, i) => WeightEntryListItem(data[i]),
     );
   }
 }
--- lib/ui/widgets/weight_entry_list_item.dart
+++ lib/ui/widgets/weight_entry_list_item.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:weight_tracking/models/weight_entry.dart';
+import 'package:intl/intl.dart';
 
 class WeightEntryListItem extends StatelessWidget {
-  // TODO: Create weight entry list item
+  final WeightEntry weightEntry;
+
+  WeightEntryListItem(this.weightEntry);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Weight entry'),
+    final format = DateFormat('yyyy-MM-dd');
+
+    return ListTile(
+      title: Text(weightEntry.weight.toString()),
+      subtitle: Text(format.format(weightEntry.date)),
+      trailing: weightEntry.cheatDay ? Icon(Icons.thumb_down) : null,
     );
   }
 }
